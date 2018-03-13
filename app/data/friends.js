6569d83a72865aa5b78b711d921bc6aba0bbe57a
marilyn pagan <mpagan6@gmail.com>
2018-03-13 10:16:01 -0500

    "updated files"

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+// You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
+var friendsList= [{
+    'name': 'Stewie Griffin',
+    'photo': "https://vignette.wikia.nocookie.net/familyguy/images/7/72/Stewie_Griffin.jpg/revision/latest?cb=20130812175654",
+    'scores': [5,4,2,1,1,1,2,3,4,5]
+},
+{
+    'name':"Homer Simpson",
+    'photo': "http://entertainment.ie//images_content/rectangle/620x372/homer-header.jpg",
+    'scores': [4,1,3,2,5,4,3,2,1,5]
+},
+{
+    'name': "Bender",
+    'photo': "https://vignette.wikia.nocookie.net/villains/images/8/8f/The_Bender.jpg/revision/latest?cb=20150929221610",
+    'scores': [3,2,1,3,1,5,1,1,5,1]
+},
+{
+    'name': 'Deadpool',
+    'photo': 'http://energy106.ca/wp-content/uploads/2018/02/maxresdefault.jpg',
+    'scores': [1,3,4,4,1,1,1,1,1,1,]
+},
+{
+    'name':'Archer',
+    'photo': 'https://comedycentral.mtvnimages.com/images/shows/archer/homepage/massives/ARCHER_centered01_massive_1920x540.jpg?quality=0.85&width=1024&height=400&crop=true',
+    'scores': [2,5,5,5,4,3,1,2,4,4]
+
+}
+]
+
+module.exports= friendsList;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,46 @@
+//A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+//A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+var express = require("express");
+var path = require("path");
+var router = express.Router();
+var friendsList = require('../data/friends.js');
+
+// middleware specific to this router
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now());
+    next();
+});
+
+router.post('/data/friends', function(req, res) {
+    let newSurvey = req.body;
+    let pickedFriend;
+    let friendCalc = [];
+    for (var i = 0; i < friendsList.length; i++) {
+        var totalDifference = 0;
+        for (var k = 0; k < 10; k++) {
+            let scoreDiff = Math.abs(friendsList[i].scores[k] - newSurvey.scores[k]);
+            totalDifference += scoreDiff;
+        }
+        friendCalc.push({
+            name: friendsList[i].name,
+            picture: friendsList[i].picture,
+            totalDiff: totalDifference
+        });
+    }
+    let maxScore = 40;
+    friendCalc.map(function(obj) {
+        if (obj.totalDiff < maxScore) maxScore = obj.totalDiff;
+    });
+    pickedFriend = friendCalc.filter(function(e) { return e.totalDiff == maxScore; });
+
+    res.json(pickedFriend);
+    friendsList.push(newSurvey);
+
+});
+
+router.get('/data/friends', function(req, res) {
+    res.json(friendsList);
+});
+
+module.exports = router;
